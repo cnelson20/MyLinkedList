2238b9af1780273622c87762e697a275747787fa
cnelson20 <cjnelson2004@gmail.com>
2021-01-05 14:24:54 -0500

    updated ToString i think

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,24 +2,44 @@ public class MyLinkedList {
   private int length;
   private MyNode head, tail;
 
+
+  public MyLinkedList() {
+    length = 0;
+    head = null;
+    tail = head;
+  }
+
+
   public boolean add(String value) {
     MyNode v = new MyNode(value,tail,null);
-    tail.setNext(v);
+    if (length == 0) {
+      head = v;
+      tail = head;
+    } else {
+      tail.setNext(v);
+      tail = v;
+    }
+    length++;
     return true;
   }
   public boolean add(int index, String value) {
+    if (length == 0) {
+      return add(value);
+    }
     MyNode pr = get(index);
     MyNode nxt = pr.getNext();
     MyNode newNode = new MyNode(value,pr,nxt);
     pr.setNext(newNode);
     nxt.setPrev(newNode);
 
+    length++;
     return true;
   }
   public String toString() {
     MyNode current = head;
     String ret = "";
     while (current != null) {
+      if (current != head) {ret += ",";}
       ret += current.getValue();
       current = current.getNext();
     }
